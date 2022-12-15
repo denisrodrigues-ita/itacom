3eadaeaa80b39d3919e1eb32a8771217b1225788
denis rodrigues <denisrodrigues.ita@gmail.com>
2022-12-15 18:14:49 -0300

    footer

--- src/public/Footer.jsx
+++ src/public/Footer.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import { AiOutlineMail, AiOutlineWhatsApp } from "react-icons/ai";
+import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
-    <footer>
-      Footer
+    <footer className={styles.footer}>
+      <div>
+        <p>Entre em contato</p>
+        <p>E tenha sua empresa sempre ao alcance do público</p>
+        <address>
+          <p>
+            Email: denisrodrigues.ita@gmail.com <AiOutlineMail />
+          </p>
+          <p>
+            <a href={`https://wa.me/+5535984484087`} target="_blank">
+              (35) 98448-4087 <AiOutlineWhatsApp />
+            </a>
+          </p>
+        </address>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/public/Header.jsx
+++ src/public/Header.jsx
@@ -1,38 +1,57 @@
-import React from 'react'
-import logo from '../img/logo.png'
-import { Link } from 'react-router-dom'
-import styles from './Header.module.css'
-import Search from '../components/Search'
+import React from "react";
+import logo from "../img/logo.png";
+import { Link } from "react-router-dom";
+import styles from "./Header.module.css";
+import Search from "../components/Search";
 
 const Header = () => {
   return (
     <header>
       <div className={styles.grid}>
         <div>
-          <Link to="/"><img src={logo} alt="logo" /></Link>
+          <Link to="/">
+            <img src={logo} alt="logo" />
+          </Link>
         </div>
         <div className={styles.flx}>
           <nav>
             <ul>
-              <li><Link to="/">Página Inicial</Link></li>
-              <li><a>Procurar Por</a>
+              <li>
+                <Link to="/">Página Inicial</Link>
+              </li>
+              <li>
+                <a>Procurar Por</a>
                 <ul>
-                  <li><Link to="/academias">Academias</Link></li>
-                  <li><Link to="/farmacias">Farmácias</Link></li>
-                  <li><Link to="/mercados">Mercados</Link></li>
-                  <li><Link to="/padarias">Padarias</Link></li>
-                  <li><Link to="/restaurantes">Restaurantes</Link></li>
-                  <li><Link to="/servicos">Serviços</Link></li>
+                  <li>
+                    <Link to="/academias">Academias</Link>
+                  </li>
+                  <li>
+                    <Link to="/farmacias">Farmácias</Link>
+                  </li>
+                  <li>
+                    <Link to="/mercados">Mercados</Link>
+                  </li>
+                  <li>
+                    <Link to="/padarias">Padarias</Link>
+                  </li>
+                  <li>
+                    <Link to="/restaurantes">Restaurantes</Link>
+                  </li>
+                  <li>
+                    <Link to="/servicos">Serviços</Link>
+                  </li>
                 </ul>
               </li>
-              <li><Link to="/contato">Contato</Link></li>
+              <li>
+                <Link to="/contato">Contato</Link>
+              </li>
             </ul>
           </nav>
           <Search />
         </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
