13955e05b554447dfddc1d77ebac02b04b22e652
denis rodrigues <denisrodrigues.ita@gmail.com>
2022-12-20 23:13:50 -0300

    Update Busca.jsx

--- src/Pages/Busca.jsx
+++ src/Pages/Busca.jsx
@@ -1,12 +1,37 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import emp from "../../Api/";
 
 const Busca = () => {
   const { pesquisa } = useParams();
+  const [search, setSearch] = React.useState("");
+
+  let busca = [];
+  React.useEffect(() => {
+    Object.entries(emp).forEach((element) => {
+      element[1].forEach((e) => {
+        if (e.nome.toLowerCase().includes(pesquisa.toLowerCase())) {
+          busca.push(e);
+        }
+        setSearch(busca);
+      });
+    });
+  }, [pesquisa]);
+  console.log(search);
 
   return (
     <section>
-      <p>busca</p>
+      {search.length === 0 ? (
+        <p>Nenhuma resultado encontrado</p>
+      ) : (
+        search.map((e) => (
+          <div key={e.nome}>
+            <img src={e.img} />
+            <p>{e?.nome}</p>
+            <p>{e?.telefone}</p>
+          </div>
+        ))
+      )}
     </section>
   );
 };
