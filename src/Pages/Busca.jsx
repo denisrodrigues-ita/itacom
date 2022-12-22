6fdba4e3d437ba09706195eb9b16c827cde81fdf
denis rodrigues <denisrodrigues.ita@gmail.com>
2022-12-22 18:27:40 -0300

    search finalizado

--- src/Pages/Busca.jsx
+++ src/Pages/Busca.jsx
@@ -1,10 +1,39 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import emp from "../../Api/";
+import styles from "./Busca.module.css";
+import Modal from "react-modal";
+import {
+  AiOutlineClose,
+  AiFillFacebook,
+  AiFillInstagram,
+  AiOutlineWhatsApp,
+} from "react-icons/ai";
+import { Link } from "react-router-dom";
+
+let arr = [];
+
+// Modal react empresas
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+Modal.setAppElement("#root");
 
 const Busca = () => {
+  let subtitle;
   const { pesquisa } = useParams();
   const [search, setSearch] = React.useState([]);
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+  const [modal, setModal] = React.useState("");
+  const [modalEmpresa, setModalEmpresa] = React.useState({});
 
   React.useEffect(() => {
     let busca = [];
@@ -20,17 +49,93 @@ const Busca = () => {
       });
     });
   }, [pesquisa]);
-  console.log(search);
+
+  const openModal = ({ currentTarget }) => {
+    setIsOpen(true);
+    setModal(currentTarget.querySelector("#h3").textContent);
+  };
+
+  const afterOpenModal = () => {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = "#f00";
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  // Função para pegar o nome da empresa que foi clicada
+  React.useEffect(() => {
+    search.forEach((e) => {
+      if (e.nome === modal) {
+        setModalEmpresa(e);
+      }
+    });
+  }, [modal]);
 
   return (
-    <section>
+    <section className={`container ${styles.busca}`}>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        // contentLabel="Example Modal"
+      >
+        <div
+          id="modal"
+          className={styles.modal}
+          ref={(_subtitle) => (subtitle = _subtitle)}
+        >
+          <button className={styles.closed} onClick={closeModal}>
+            <AiOutlineClose />
+          </button>
+          <div>
+            <img src={modalEmpresa?.img} alt={modalEmpresa?.nome} />
+          </div>
+          <div>
+            <p>{modalEmpresa?.nome}</p>
+            <p>{modalEmpresa?.cidade}</p>
+            <p>{modalEmpresa?.endereco}</p>
+            <p>{modalEmpresa?.telefone}</p>
+            <div className={styles.socialMedias}>
+              <div>
+                <p>
+                  <a href={modalEmpresa?.facebook} target="_blank">
+                    <AiFillFacebook />
+                  </a>
+                </p>
+                <p>
+                  <a href={modalEmpresa?.instagram} target="_blank">
+                    <AiFillInstagram />
+                  </a>
+                </p>
+                <p>
+                  <a
+                    href={`https://wa.me/${modalEmpresa?.whatsapp}`}
+                    target="_blank"
+                  >
+                    <AiOutlineWhatsApp />
+                  </a>
+                </p>
+              </div>
+              <div>
+                <Link to={`/${modalEmpresa.tipo}/${modalEmpresa.nome}`}>
+                  <button>Ver Mais</button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Modal>
+
       {search.length === 0 ? (
         <p>Nenhuma resultado encontrado</p>
       ) : (
         search.map((e) => (
-          <div key={e.nome}>
+          <div className={styles.a} key={e.nome} onClick={openModal}>
             <img src={e.img} />
-            <p>{e?.nome}</p>
+            <h3 id="h3">{e?.nome}</h3>
             <p>{e?.telefone}</p>
           </div>
         ))
